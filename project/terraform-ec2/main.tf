0cdcf80e685901b4b612adcf2d532dfac65e5d42
Amar <amarsaiyellanki789@gmail.com>
2026-02-24 13:40:59 +0530

    Message

--- project/terraform-ec2/main.tf
+++ project/terraform-ec2/main.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "allow_my_ip"{
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_block = "3.87.88.135"
+        cidr_block = 3.87.88.135
         ipv6_cidr_blocks = ["::/0"]
     }
 }
