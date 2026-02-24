e323c261e5e48250a0fea7d28d466d57cb588cdb
Amar <amarsaiyellanki789@gmail.com>
2026-02-24 12:20:27 +0530

    Message

--- project/terraform-ec2/main.tf
+++ project/terraform-ec2/main.tf
@@ -24,3 +24,36 @@ resource "aws_security_group" "allow_all"{
         ipv6_cidr_blocks = ["::/0"]
     }
 }
+
+resource "aws_vpc" "main"{
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public"{
+    vpc_id = output.vpc_id
+    availability_zone = var.region
+    cidr_block = "10.0.0.0/24"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw"{
+    vpc_id = output.vpc_id
+    subnet_id = output.subnet_id
+}
+
+resource "aws_route_table" "rt"{
+    vpc_id = var.vpc_id
+}
+
+resource "aws_route" "r"{
+    route_table_id = output.aws_route_id
+    gateway_id = var.gateway_id
+    destination_cidr = "0.0.0.0/0"
+
+}
+
+resource "aws_route_table_association" "rta"{
+    route_table_id = output.aws_route_id
+    subnet_id = output.subnet_id
+}
