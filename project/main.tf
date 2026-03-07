fddf796bd4a16235c0262c3e92690c67942f0486
Amar <amarsaiyellanki789@gmail.com>
2026-03-07 14:52:03 +0530

    Commit message

--- project/main.tf
+++ project/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "qa"{
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t3.micro"
+    ami = var.ami_id
+    instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.sg.id]
 
     tags = {
